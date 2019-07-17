228e7fbf8e2192b19cf1685e398d50b2e412db1f
Petar_Grigorov <vekr0w@gmail.com>
2019-07-17 14:32:00 +0300

    Updated Travel Time with comments.

--- 08-Associative-Arrays/01-Associative-Arrays-Exercise/06-Travel-Time.js
+++ 08-Associative-Arrays/01-Associative-Arrays-Exercise/06-Travel-Time.js
@@ -7,9 +7,11 @@ function travelTime(input) {
     currentPrice = Number(currentPrice);
 
     if (!countryTownPriceData.hasOwnProperty(country)) {
+      //create new property if not present
       countryTownPriceData[country] = {};
       countryTownPriceData[country][town] = currentPrice;
     } else {
+      //update existing property
       let countryObject = countryTownPriceData[country];
       if (!countryObject.hasOwnProperty(town)) {
         countryObject[town] = currentPrice;
@@ -22,7 +24,7 @@ function travelTime(input) {
     }
 
   }
-
+//sort by country name(Alphabetically)
   let sortedCountryData = Object.entries(countryTownPriceData)
     .sort((a, b) => {
       let firstCountryName = a[0];
@@ -31,12 +33,14 @@ function travelTime(input) {
     });
 
   for (const [countryName, townData] of sortedCountryData) {
+    //sort by price Low to High
     let sortedTownData = Object.entries(townData)
       .sort((a, b) => {
         let firstPrice = a[1];
         let secondPrice = b[1];
         return firstPrice - secondPrice;
       });
+      //create output on a single line to log it.
     let output = `${countryName} -> `;
     for (const [townName, price] of sortedTownData) {
       output += `${townName} -> ${price} `;
