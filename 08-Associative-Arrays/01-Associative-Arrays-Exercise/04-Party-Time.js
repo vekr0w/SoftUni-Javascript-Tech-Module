dd24a5a8e47f7d472037c8450269470fbb57101c
Petar_Grigorov <vekr0w@gmail.com>
2019-07-17 15:10:05 +0300

    Party time ready 80/100. Updated the Not-Finished file.

--- 08-Associative-Arrays/01-Associative-Arrays-Exercise/04-Party-Time.js
+++ 08-Associative-Arrays/01-Associative-Arrays-Exercise/04-Party-Time.js
@@ -3,18 +3,37 @@ function sortGuests(input) {
   let data = input.join(' ').split(' PARTY ');
   let guestList = data.shift().split(' ');
   let incomingGuests = data.shift().split(' ');
-  for (let i = 0; i < incomingGuests.length; i++) {
-    const currentGuest = incomingGuests[i];
-    const currentGuestIndex = guestList.indexOf(currentGuest);
-    if (currentGuestIndex != -1) {
-      guestList.splice(currentGuestIndex, 1);
+
+  let sortedGuestList = {
+    vip: [],
+    regular: []
+  };
+
+  for (const guest of guestList) {
+    if (Number(guest[0])) {
+      sortedGuestList.vip.push(guest);
+    } else {
+      sortedGuestList.regular.push(guest);
     }
   }
-  guestList.sort();
-  console.log(guestList.length);
-  for (const guest of guestList) {
-    console.log(guest);
+
+  for (let incGuest of incomingGuests) {
+
+    if (Number(incGuest[0])) {
+      let vipList = sortedGuestList.vip;
+      let indexOfIncGuest = vipList.indexOf(incGuest);
+      vipList.splice(indexOfIncGuest, 1);
+    } else {
+      let regularList = sortedGuestList.regular;
+      let indexOfIncGuest = regularList.indexOf(incGuest);
+      regularList.splice(indexOfIncGuest, 1);
+    }
   }
+
+  let guestsLeft = sortedGuestList.vip.length + sortedGuestList.regular.length;
+  console.log(guestsLeft);
+  console.log(sortedGuestList.vip.join('\n'));
+  console.log(sortedGuestList.regular.join('\n'));
 }
 sortGuests(
   ['7IK9Yo0h',
@@ -22,13 +41,11 @@ sortGuests(
     'Ce8vwPmE',
     'SVQXQCbc',
     'tSzE5t0p',
-    '7IK9Yo0h',
-    '8IK9Yo0h',
-    'GIK9Yo0h',
     'PARTY',
     '9NoBUajQ',
     'Ce8vwPmE',
     'SVQXQCbc'
   ]
+
 
 );
