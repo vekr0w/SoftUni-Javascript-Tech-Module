252a3c2a106e01bb572a5db5765db9111c09c78d
Petar_Grigorov <vekr0w@gmail.com>
2019-07-22 11:18:12 +0300

    Pascal-Case-Splitter completed 100/100

---
+++ 09-Text-Processing/02-Text-Processing-Exercises/06-Pascal-Case-Splitter.js
@@ -0,0 +1,9 @@
+function pascalCaseSplitter(input){
+
+let text = input.replace(/([A-Z])/g, ",$1");
+text = text.split(',').filter(Boolean);
+console.log(text.join(', '));
+
+
+}
+pascalCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan');
